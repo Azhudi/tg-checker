5291fc4eb9a412d6c4ede3ca002d6117649e5ac7
Azhudi <67905799+Azhudi@users.noreply.github.com>
2020-07-07 10:06:53 +0300

    Add files via upload

---
+++ __code__.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from telethon.sync import TelegramClient
+from telethon import functions, types
+import random
+
+line = "___________"
+
+name = 'Botik'
+api_id = 1313116
+api_hash = 'f01ef8f85a36c139d4777f905ad8bb54'
+
+def check(phone):
+    if phone[0] == '+' and len(phone) >= 10:
+        with TelegramClient(name, api_id, api_hash) as client:
+            contacts = [types.InputPhoneContact(
+                client_id=random.randrange(-2**63, 2**63),
+                phone=phone,
+                first_name='Name',
+                last_name='(checker)')]
+            result = str(client(functions.contacts.ImportContactsRequest(contacts)))
+            if result.find('username=') != -1:
+                username = client.get_entity(phone).username
+                if username == None:
+                    print(line+'\n'*2+"Номер найден, но у пользователя отсутствует username!"+'\n'+line+'\n')
+                    print("User info: "+result+'\n'+line+'\n')
+                else:
+                    result = client(functions.contacts.DeleteContactsRequest(id=[username]))
+                    print(line+'\n'*2+phone+" - номер найден! Пользователь известен как: @"+username+'\n'+line+'\n')
+            else: print(line+'\n'*2+phone+" - номер не зарегестрирован в тг!"+'\n'+line+'\n')
+    else: print(line+'\n'*2+phone+" - номер некорректен! Введите номер в международном формате!"+'\n'+line+'\n')
+
+def singleMode(): 
+    check(input("\nНомер для проверки: "))
+
+def multiMode():
+    for phone in open('phones.txt', 'r').read().split(): check(phone)
